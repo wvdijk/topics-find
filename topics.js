692515f11c97140f2ab3700176bc232b9b846006
Wieland van Dijk <wvdijk@gmail.com>
2018-09-27 09:31:54 +0200

    Add files via upload
    Initial commit

---
+++ topics.js
@@ -0,0 +1,10 @@
+javascript: (function(){
+  var pagetitle = document.title;
+  if (window.location.hostname.indexOf('parool.nl') > -1) {
+    var headline = pagetitle.split(" - ")[0];
+  } else {
+    var headline = pagetitle.split(" | ")[0];
+  }
+  var searchString = encodeURIComponent(headline);
+  var url = 'http://www.topics.nl/zoek/?query=' + searchString; window.open(url, '_blank');
+})();
