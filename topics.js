083e5a769fac6772970ee43aa80b5b413af74ec6
Dijk <w.vandijk@nrc.nl>
2019-06-11 17:33:25 +0200

    Het Parool now uses pipes in document.title as well, removed exception

--- topics.js
+++ topics.js
@@ -1,11 +1,7 @@
 javascript: (function(){
   var pagetitle = document.title;
-  if (window.location.hostname.indexOf('parool.nl') > -1) {
-    var headline = pagetitle.split(" - ")[0];
-  } else {
-    var headline = pagetitle.split(" | ")[0];
-  }
-  var searchString = encodeURIComponent(headline);
-  var url = 'http://www.topics.nl/zoek/?query=' + searchString;
+  let headline = pagetitle.split(" | ")[0];
+  let searchString = encodeURIComponent(headline);
+  let url = 'http://www.topics.nl/zoek/?query=' + searchString;
   window.open(url, '_blank');
 })();
